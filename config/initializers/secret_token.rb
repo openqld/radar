7cfd3c14e70cce62041e4ea0c607f390236fe540
Paul O'Keeffe <paul.okeeffe@columina.com>
2012-11-26 23:49:00 +1000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radar::Application.config.secret_token = '7aa7905c47e0dae74becc09d72c454e79a0bea07932b3da3633441a2e0a5043eae5dd2cc1162296ebcdc8cef4911b3d2bf7fcf924b2f23556572d275861b3dc2'
